1c63e31bc562fcce8c517cc98063adb0bdb1d03c
deonash <40314332+deonash@users.noreply.github.com>
2019-03-20 16:12:58 +0530

    Text to Speech Examples

---
+++ Powershell/Speech/Welcome.ps1
@@ -0,0 +1,6 @@
+﻿Add-Type -AssemblyName System.speech 
+
+$speak = New-Object System.Speech.Synthesis.SpeechSynthesizer 
+$speak.speak('Welcome to TechInn, I hope you are enjoying fun with Powershell scripting')
+
+
