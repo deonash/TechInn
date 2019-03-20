ba21f624152b419490ad6876df97f7fb67333a3f
deonash <40314332+deonash@users.noreply.github.com>
2019-03-20 16:11:42 +0530

    Delete Welcome.ps1

--- Speach/Welcome.ps1
+++
@@ -1,6 +0,0 @@
-﻿Add-Type -AssemblyName System.speech 
-
-$speak = New-Object System.Speech.Synthesis.SpeechSynthesizer 
-$speak.speak('Welcome to TechInn, I hope you are enjoying fun with Powershell scripting')
-
-
