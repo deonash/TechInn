9237c07a05e8abbc5bbbc1c243b3f502c366304f
deonash <40314332+deonash@users.noreply.github.com>
2019-03-21 15:50:14 +0530

    Add files via upload

---
+++ Powershell/UI/SimpleForm.ps1
@@ -0,0 +1,82 @@
+﻿Add-Type -AssemblyName System.Windows.Forms
+Add-Type -AssemblyName System.Drawing
+
+$form = New-Object System.Windows.Forms.Form
+$form.Text = 'Data Entry Form'
+$form.Size = New-Object System.Drawing.Size(300,200)
+$form.StartPosition = 'CenterScreen'
+
+$OKButton = New-Object System.Windows.Forms.Button
+$OKButton.Location = New-Object System.Drawing.Point(75,120)
+$OKButton.Size = New-Object System.Drawing.Size(75,23)
+$OKButton.Text = 'OK'
+$OKButton.DialogResult = [System.Windows.Forms.DialogResult]::OK
+$form.AcceptButton = $OKButton
+$form.Controls.Add($OKButton)
+
+$CancelButton = New-Object System.Windows.Forms.Button
+$CancelButton.Location = New-Object System.Drawing.Point(150,120)
+$CancelButton.Size = New-Object System.Drawing.Size(75,23)
+$CancelButton.Text = 'Cancel'
+$CancelButton.DialogResult = [System.Windows.Forms.DialogResult]::Cancel
+$form.CancelButton = $CancelButton
+$form.Controls.Add($CancelButton)
+
+$label = New-Object System.Windows.Forms.Label
+$label.Location = New-Object System.Drawing.Point(10,20)
+$label.Size = New-Object System.Drawing.Size(280,20)
+$label.Text = 'Please enter the sentense in the space below:'
+$form.Controls.Add($label)
+
+$textBox = New-Object System.Windows.Forms.TextBox
+$textBox.Location = New-Object System.Drawing.Point(10,40)
+$textBox.Size = New-Object System.Drawing.Size(260,20)
+$form.Controls.Add($textBox)
+
+$form.Topmost = $true
+
+$form.Add_Shown({$textBox.Select()})
+$result = $form.ShowDialog()
+
+if ($result -eq [System.Windows.Forms.DialogResult]::OK)
+{
+    $x = $textBox.Text
+    $x
+
+    $outform = New-Object System.Windows.Forms.Form
+    $outform.Text = 'Data Output Form'
+    $outform.Size = New-Object System.Drawing.Size(300,200)
+    $outform.StartPosition = 'CenterScreen'
+
+    $outlabel = New-Object System.Windows.Forms.Label
+    $outlabel.Location = New-Object System.Drawing.Point(10,20)
+    $outlabel.Size = New-Object System.Drawing.Size(300,40)
+    $outlabel.Text = $x
+    $outform.Controls.Add($outlabel)
+
+    $OKButton = New-Object System.Windows.Forms.Button
+    $OKButton.Location = New-Object System.Drawing.Point(20,100)
+    $OKButton.Size = New-Object System.Drawing.Size(70,30)
+    $OKButton.Text = 'OK'
+    $OKButton.DialogResult = [System.Windows.Forms.DialogResult]::OK
+    $outform.AcceptButton = $OKButton
+    $outform.Controls.Add($OKButton)
+
+    $CancelButton = New-Object System.Windows.Forms.Button
+    $CancelButton.Location = New-Object System.Drawing.Point(120,100)
+    $CancelButton.Size = New-Object System.Drawing.Size(70,30)
+    $CancelButton.Text = 'Cancel'
+    $CancelButton.DialogResult = [System.Windows.Forms.DialogResult]::Cancel
+    $outform.CancelButton = $CancelButton
+    $outform.Controls.Add($CancelButton)
+
+    $outform.Topmost = $true
+
+    $result = $outform.ShowDialog()
+    if ($result -eq [System.Windows.Forms.DialogResult]::OK)
+    {
+     
+        Write-Host "Thanks!!!!"
+    }
+} 
+
